a3de3cc74416abe1c72effe845f9b2c231b5dfcb
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-12-23 14:52:49 +0400

    text fitting added

--- scripts/view.js
+++ scripts/view.js
@@ -26,6 +26,7 @@ const view = {
         $("#question p").animate({"margin-bottom": "26px", opacity: 0}, 350);
         await timeout(350);
         $("#question p").html(text);
+        view.fitText($("#question p"), 91, 18, 24);
         await timeout(60);
         $("#question p").css({"margin-bottom": 0, "margin-top": "26px", opacity: 0});
         $("#question p").animate({"margin-top": 0, "margin-bottom": "0px", opacity: 1}, 350);
@@ -62,6 +63,15 @@ const view = {
         await timeout(200);
 
         $("#bigPlay").removeClass("under"); await timeout(500);
+    },
+
+    fitText: async (element, height, defaultFontSize, defaultLineHeight) => {
+        if (element.height() > height) {
+            element.css({"font-size": "-=0.5px", "line-height": "-=0.5px"});
+            view.fitText(element, height);
+        } else {
+            element.css({"font-size": defaultFontSize, "line-height": `${defaultLineHeight}px`});
+        }
     },
 
     toggleBigPlay: async () => {
